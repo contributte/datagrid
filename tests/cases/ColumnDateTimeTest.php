ad9317139b6adae739176b49fee9e82c93d8e96a
Pavel Janda <me@paveljanda.com>
2015-12-15 23:02:11 +0100

    Tests: ColumnDateTime

---
+++ tests/cases/ColumnDateTimeTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Ublaboo\DataGrid\Tests\Cases;
+
+use Tester\TestCase,
+	Tester\Assert,
+	Mockery,
+	Ublaboo\DataGrid\DataGrid,
+	Ublaboo;
+
+require __DIR__ . '/../bootstrap.php';
+require __DIR__ . '/../files/XTestingDataGridFactory.php';
+
+final class ColumnDateTimeTest extends TestCase
+{
+
+	/**
+	 * @var DataGrid
+	 */
+	private $grid;
+
+
+	public function setUp()
+	{
+		$factory = new Ublaboo\DataGrid\Tests\Files\XTestingDataGridFactory;
+		$this->grid = $factory->createXTestingDataGrid();
+	}
+
+
+	public function render($column)
+	{
+		$datetime = \DateTime::createFromFormat('Y-m-d H:i:s', '2015-12-15 22:58:42');
+		$item = ['id' => 1, 'name' => 'John', 'date' => $datetime];
+
+		return (string) $column->render($item);
+	}
+
+
+	public function testFormat()
+	{
+		/**
+		 * Defaul forma is 'j. n. Y'
+		 */
+		$datetime = $this->grid->addColumnDateTime('date', 'Date');
+		Assert::same('15. 12. 2015', $this->render($datetime));
+
+		$datetime->format('H:i:s');
+		Assert::same('22:58:42', $this->render($datetime));
+	}
+
+}
+
+
+$test_case = new ColumnDateTimeTest;
+$test_case->run();
