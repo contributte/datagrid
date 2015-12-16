42fe296fdf8f54f1f86f1642f86fceadfbb3b863
Pavel Janda <me@paveljanda.com>
2015-12-16 21:26:26 +0100

    Better datetime recognition

--- src/Column/ColumnDateTime.php
+++ src/Column/ColumnDateTime.php
@@ -25,17 +25,34 @@ class ColumnDateTime extends Column
 			 * Try to convert string Y-m-d to DateTime object
 			 */
 			$date = \DateTime::createFromFormat('Y-m-d', $value);
-
 			if ($date) {
 				return $date->format($this->format);
 			}
 
 			/**
-			 * Try to convert string Y-m-d to DateTime object
+			 * Try to convert string Y-m-d H:i:s to DateTime object
 			 */
 			$date = \DateTime::createFromFormat('Y-m-d H:i:s', $value);
-
 			if ($date) {
+				return $date->format($this->format);
+			}
+
+			/**
+			 * Try to convert string Y-m-d H:i:s.u to DateTime object
+			 */
+			$date = \DateTime::createFromFormat('Y-m-d H:i:s.u', $value);
+			if ($date) {
+				return $date->format($this->format);
+			}
+
+			/**
+			 * Try strtotime
+			 */
+			$timestamp = strtotime($value);
+			if (FALSE !== $timestamp) {
+				$date = new \DateTime;
+				$date->setTimestamp($timestamp);
+
 				return $date->format($this->format);
 			}
 
