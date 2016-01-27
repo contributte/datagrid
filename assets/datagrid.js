2c3676c2f4c9934a1837fcf733a0ff1bdc9a1a93
Pavel Janda <me@paveljanda.com>
2016-01-27 09:24:03 +0100

    Assets build

--- assets/datagrid.js
+++ assets/datagrid.js
@@ -105,7 +105,6 @@ datagridSortable = function() {
         type: 'GET',
         url: url,
         error: function(jqXHR, textStatus, errorThrown) {
-          console.log(jqXHR.statusText);
           return alert(jqXHR.statusText);
         }
       });
@@ -165,6 +164,51 @@ $.nette.ext('datagrid.confirm', {
       if (confirm_message) {
         return confirm(confirm_message);
       }
+    }
+  }
+});
+
+$(document).on('click', '.datagrid [data-toggle-detail]', function() {
+  var grid, id, row;
+  id = $(this).attr('data-toggle-detail');
+  grid = $(this).attr('data-toggle-detail-grid');
+  return row = $('.' + grid + ' .item-detail-' + id);
+});
+
+$.nette.ext('datargid.item_detail', {
+  before: function(xhr, settings) {
+    var id, row_detail;
+    if (settings.nette && settings.nette.el.attr('data-toggle-detail')) {
+      id = settings.nette.el.attr('data-toggle-detail');
+      row_detail = $('.item-detail-' + id);
+      if (row_detail.hasClass('loaded')) {
+        if (!row_detail.find('.item-detail-content').size()) {
+          row_detail.removeClass('toggled');
+          return true;
+        }
+        if (row_detail.hasClass('toggled')) {
+          row_detail.find('.item-detail-content').slideToggle('fast', (function(_this) {
+            return function() {
+              return row_detail.toggleClass('toggled');
+            };
+          })(this));
+        } else {
+          row_detail.toggleClass('toggled');
+          row_detail.find('.item-detail-content').slideToggle('fast');
+        }
+        return false;
+      } else {
+        return row_detail.addClass('loaded');
+      }
+    }
+  },
+  success: function(payload) {
+    var id, row_detail;
+    if (payload._datagrid_toggle_detail) {
+      id = payload._datagrid_toggle_detail;
+      row_detail = $('.item-detail-' + id);
+      row_detail.toggleClass('toggled');
+      return row_detail.find('.item-detail-content').slideToggle('fast');
     }
   }
 });
