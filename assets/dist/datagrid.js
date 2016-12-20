2c93d0a90be8ae4e11c732ed0e62bc211ecff62e
Pavel Janda <me@paveljanda.com>
2016-12-20 18:09:03 +0100

    Added expanding group action selection using shift key, closes #426

--- assets/dist/datagrid.js
+++ assets/dist/datagrid.js
@@ -1,4 +1,4 @@
-var datagridFitlerMultiSelect, datagridSortable, datagridSortableTree;
+var datagridFitlerMultiSelect, datagridShiftGroupSelection, datagridSortable, datagridSortableTree;
 
 $(document).on('click', '[data-datagrid-confirm]:not(.ajax)', function(e) {
   if (!confirm($(e.target).closest('a').attr('data-datagrid-confirm'))) {
@@ -65,6 +65,62 @@ $(document).on('keydown', 'input[data-datagrid-manualsubmit]', function(e) {
     return $(this).closest('form').submit();
   }
 });
+
+datagridShiftGroupSelection = function() {
+  var last_checkbox;
+  last_checkbox = null;
+  return document.addEventListener('click', function(e) {
+    var checkboxes_rows, current_checkbox_row, el, event, i, ie, input, j, k, last_checkbox_row, last_checkbox_tbody, len, len1, len2, ref, ref1, results, row, rows;
+    ref = e.path;
+    for (i = 0, len = ref.length; i < len; i++) {
+      el = ref[i];
+      if ($(el).is('.col-checkbox') && last_checkbox && e.shiftKey) {
+        current_checkbox_row = $(el).closest('tr');
+        last_checkbox_row = last_checkbox.closest('tr');
+        last_checkbox_tbody = last_checkbox_row.closest('tbody');
+        checkboxes_rows = last_checkbox_tbody.find('tr').toArray();
+        if (current_checkbox_row.index() > last_checkbox_row.index()) {
+          rows = checkboxes_rows.slice(last_checkbox_row.index(), current_checkbox_row.index());
+        } else if (current_checkbox_row.index() < last_checkbox_row.index()) {
+          rows = checkboxes_rows.slice(current_checkbox_row.index() + 1, last_checkbox_row.index());
+        }
+        if (!rows) {
+          return;
+        }
+        for (j = 0, len1 = rows.length; j < len1; j++) {
+          row = rows[j];
+          input = $(row).find('.col-checkbox input[type=checkbox]')[0];
+          if (input) {
+            input.checked = true;
+            ie = window.navigator.userAgent.indexOf("MSIE ");
+            if (ie) {
+              event = document.createEvent('Event');
+              event.initEvent('change', true, true);
+            } else {
+              event = new Event('change', {
+                'bubbles': true
+              });
+            }
+            input.dispatchEvent(event);
+          }
+        }
+      }
+    }
+    ref1 = e.path;
+    results = [];
+    for (k = 0, len2 = ref1.length; k < len2; k++) {
+      el = ref1[k];
+      if ($(el).is('.col-checkbox')) {
+        results.push(last_checkbox = $(el));
+      } else {
+        results.push(void 0);
+      }
+    }
+    return results;
+  });
+};
+
+datagridShiftGroupSelection();
 
 document.addEventListener('change', function(e) {
   var at_least_one, event, grid, i, ie, input, inputs, len, results, select;
--- assets/src/datagrid.coffee
+++ assets/src/datagrid.coffee
@@ -64,6 +64,53 @@ $(document).on('keydown', 'input[data-datagrid-manualsubmit]', (e) ->
 		$(this).closest('form').submit()
 )
 
+
+datagridShiftGroupSelection = ->
+	last_checkbox = null
+
+	document.addEventListener 'click', (e) ->	
+		for el in e.path
+			if $(el).is('.col-checkbox') && last_checkbox && e.shiftKey
+				current_checkbox_row = $(el).closest('tr')
+
+				last_checkbox_row = last_checkbox.closest('tr')
+				last_checkbox_tbody = last_checkbox_row.closest('tbody')
+
+				checkboxes_rows = last_checkbox_tbody.find('tr').toArray()
+
+				if current_checkbox_row.index() > last_checkbox_row.index()
+					rows = checkboxes_rows.slice(last_checkbox_row.index(), current_checkbox_row.index())
+
+				else if current_checkbox_row.index() < last_checkbox_row.index()
+					rows = checkboxes_rows.slice(current_checkbox_row.index() + 1, last_checkbox_row.index())
+
+				if !rows
+					return
+
+				for row in rows
+					input = $(row).find('.col-checkbox input[type=checkbox]')[0]
+
+					if input
+						input.checked = true
+
+						ie = window.navigator.userAgent.indexOf("MSIE ")
+
+						if ie
+							event = document.createEvent('Event')
+							event.initEvent('change', true, true);
+						else
+							event = new Event('change', {'bubbles': true})
+
+						input.dispatchEvent(event)
+					
+				
+		for el in e.path
+			if $(el).is('.col-checkbox')
+				last_checkbox = $(el)
+
+datagridShiftGroupSelection()
+
+
 document.addEventListener 'change', (e) ->
 	grid = e.target.getAttribute('data-check')
 
