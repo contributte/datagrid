d8ccda296d3b0d120131b9e0e3a61b4a495e98a0
j.patus <j.patus@simpledino.com>
2017-05-15 19:16:04 +0200

    Support groupped nette database table data source (inspired by https://forum.nette.org/cs/28277-zly-count-ublaaboo-datagrid-pri-nettedatabasetabledatasource-s-groupovanim-stlpcu-a-countom-nad-danym-stlpcom, but support of multiple groupped columns added)

--- src/DataSource/NetteDatabaseTableDataSource.php
+++ src/DataSource/NetteDatabaseTableDataSource.php
@@ -59,10 +59,18 @@ class NetteDatabaseTableDataSource extends FilterableDataSource implements IData
 		} catch (\LogicException $e) {
 			return $this->data_source->count('*');
 		}
+                
+                $dataSourceSqlBuilder = $this->data_source->getSqlBuilder();
 
-		return $this->data_source->count(
-			$this->data_source->getName() . '.' . (is_array($primary) ? reset($primary) : $primary)
-		);
+                if($dataSourceSqlBuilder->getGroup() != '') {
+                    return $this->data_source->count(
+                        'DISTINCT ' . \Nette\Utils\Strings::replace($dataSourceSqlBuilder->getGroup(), '~ (DESC|ASC)~')
+                    );
+                } else {
+                    return $this->data_source->count(
+                        $this->data_source->getName() . '.' . (is_array($primary) ? reset($primary) : $primary)
+                    );
+                }
 	}
 
 
