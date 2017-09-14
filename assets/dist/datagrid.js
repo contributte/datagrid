5be21c19c448753d6fb13a69d7b3265119e6e852
Filip Lukac <filip.lukac@gmail.com>
2017-09-14 14:24:35 +0200

    Use single quotes when finding option in inline edit
    Fixes #572 issue
    
    Signed-off-by: Filip Lukac <filip.lukac@gmail.com>

--- assets/dist/datagrid.js
+++ assets/dist/datagrid.js
@@ -545,7 +545,7 @@ $(document).on('click', '[data-datagrid-editable-url]', function(event) {
       input = $(cell.data('datagrid-editable-element'));
       input.find('option').each(function() {
         if ($(this).text() === valueToEdit) {
-          return input.find('option[value=' + valueToEdit + ']').prop('selected', true);
+          return input.find("option[value='" + valueToEdit + "']").prop('selected', true);
         }
       });
     } else {
@@ -571,7 +571,7 @@ $(document).on('click', '[data-datagrid-editable-url]', function(event) {
           method: 'POST',
           success: function(payload) {
             if (cell.data('datagrid-editable-type') === 'select') {
-              cell.html(input.find('option[value=' + value + ']').html());
+              cell.html(input.find("option[value='" + value + "']").html());
             } else {
               if (payload._datagrid_editable_new_value) {
                 value = payload._datagrid_editable_new_value;
