04518b8bfbb826df981626ff77640def80ea5c0b
Pavel Janda <me@paveljanda.com>
2018-01-12 09:48:20 +0100

    Fixed sorting: let the previous remain after filtering

--- src/DataGrid.php
+++ src/DataGrid.php
@@ -556,7 +556,6 @@ class DataGrid extends Nette\Application\UI\Control
 		/**
 		 * Set template file and render it
 		 */
-		\Tracy\Debugger::barDump($this->getTemplateFile());
 		$template->setFile($this->getTemplateFile());
 		$template->render();
 	}
