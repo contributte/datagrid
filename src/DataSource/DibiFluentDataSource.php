f504afd18d93ce273498df2e682a3f2d2be82512
BigOHenry <o.sochurek@gmail.com>
2018-03-10 10:39:46 +0100

    Old DibiFluent replaced by Dibi\Fluent
    Old DibiFluent replaced by Dibi\Fluent

--- src/DataSource/DibiFluentDataSource.php
+++ src/DataSource/DibiFluentDataSource.php
@@ -9,7 +9,6 @@
 namespace Ublaboo\DataGrid\DataSource;
 
 use Dibi;
-use DibiFluent;
 use Ublaboo\DataGrid\AggregationFunction\IAggregatable;
 use Ublaboo\DataGrid\Filter;
 use Ublaboo\DataGrid\Utils\DateTimeHelper;
@@ -19,7 +18,7 @@ class DibiFluentDataSource extends FilterableDataSource implements IDataSource, 
 {
 
 	/**
-	 * @var DibiFluent
+	 * @var Dibi\Fluent
 	 */
 	protected $data_source;
 
@@ -35,10 +34,10 @@ class DibiFluentDataSource extends FilterableDataSource implements IDataSource, 
 
 
 	/**
-	 * @param DibiFluent $data_source
+	 * @param Dibi\Fluent $data_source
 	 * @param string $primary_key
 	 */
-	public function __construct(DibiFluent $data_source, $primary_key)
+	public function __construct(Dibi\Fluent $data_source, $primary_key)
 	{
 		$this->data_source = $data_source;
 		$this->primary_key = $primary_key;
@@ -285,7 +284,7 @@ class DibiFluentDataSource extends FilterableDataSource implements IDataSource, 
 			 */
 			$this->data_source->clause('ORDER BY');
 
-			$reflection = new \ReflectionClass('DibiFluent');
+			$reflection = new \ReflectionClass('Dibi\Fluent');
 			$cursor_property = $reflection->getProperty('cursor');
 			$cursor_property->setAccessible(true);
 			$cursor = $cursor_property->getValue($this->data_source);
