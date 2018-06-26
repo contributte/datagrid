e05497ff7653bad5db9a62a48f41348259f4d293
Honza Kosák <kosak@webovy-servis.cz>
2018-06-26 17:55:23 +0200

    fixed redraw item

--- assets/dist/datagrid.js
+++ assets/dist/datagrid.js
@@ -734,7 +734,7 @@ $.nette.ext('datagrid.inline-editing', {
 $.nette.ext('datagrid.redraw-item', {
   success: function(payload) {
     var row;
-    if (payload._datagrid_redraw_item_class) {
+    if (payload._datagrid_redraw_item_class !== undefined) {
       row = $('tr[data-id=' + payload._datagrid_redraw_item_id + ']');
       return row.attr('class', payload._datagrid_redraw_item_class);
     }
