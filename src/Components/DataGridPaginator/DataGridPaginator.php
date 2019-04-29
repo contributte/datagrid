d02b585d9ba80601c6fde8877c13473e12f72dc5
Václav Pelíšek <peldax@gmail.com>
2019-04-29 22:58:02 +0200

    Update DataGridPaginator.php

--- src/Components/DataGridPaginator/DataGridPaginator.php
+++ src/Components/DataGridPaginator/DataGridPaginator.php
@@ -52,12 +52,8 @@ class DataGridPaginator extends Nette\Application\UI\Control
 
 	public function __construct(
 		Nette\Localization\ITranslator $translator,
-		$icon_prefix = 'fa fa-',
-		IContainer $parent = null,
-		$name = null
+		$icon_prefix = 'fa fa-'
 	) {
-		parent::__construct($parent, $name);
-
 		$this->translator = $translator;
 		$this->icon_prefix = $icon_prefix;
 	}
@@ -152,7 +148,7 @@ class DataGridPaginator extends Nette\Application\UI\Control
 	 * @param  array
 	 * @return void
 	 */
-	public function loadState(array $params)
+	public function loadState(array $params) : void
 	{
 		parent::loadState($params);
 
