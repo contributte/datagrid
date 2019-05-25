83e74b820fed77e52a50f272e714913214fd6749
Václav Pelíšek <peldax@gmail.com>
2019-05-25 13:59:01 +0200

    Update DataGrid.php

--- src/DataGrid.php
+++ src/DataGrid.php
@@ -381,19 +381,14 @@ class DataGrid extends Nette\Application\UI\Control
 	 */
 	protected $show_selected_rows_count = true;
 
-    /**
-     * @var  string
-     */
-	protected $componentFullName;
-
 	/**
 	 * @var string
 	 */
 	private $custom_paginator_template;
 
-    /**
-     * @var  string
-     */
+	/**
+	 * @var  string
+	 */
 	protected $componentFullName;
 
 	/**
