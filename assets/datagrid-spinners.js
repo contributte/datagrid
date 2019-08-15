adad11501c376137d1ba2c3995850ae20146778e
BigOHenry <o.sochurek@gmail.com>
2019-08-15 14:49:42 +0200

    Update datagrid-spinners.js

--- assets/datagrid-spinners.js
+++ assets/datagrid-spinners.js
@@ -1,6 +1,6 @@
 $.nette.ext('ublaboo-spinners', {
 	before: function(xhr, settings) {
-		var el, id, row_detail, spinner_template;
+		var el, id, row_detail, spinner_template, grid_fullname;
 		if (settings.nette) {
 			el = settings.nette.el;
 			spinner_template = $('<div class="ublaboo-spinner ublaboo-spinner-small"><i></i><i></i><i></i><i></i></div>');
@@ -8,7 +8,8 @@ $.nette.ext('ublaboo-spinners', {
 				return el.after(spinner_template);
 			} else if (el.is('.datagrid a') && el.data('toggle-detail')) {
 				id = settings.nette.el.attr('data-toggle-detail');
-				row_detail = $('.item-detail-' + id);
+				grid_fullname = settings.nette.el.attr('data-toggle-detail-grid-fullname');
+				row_detail = $('.item-detail-' + grid_fullname + '-id-' + id);
 				if (!row_detail.hasClass('loaded')) {
 					return el.addClass('ublaboo-spinner-icon');
 				}
