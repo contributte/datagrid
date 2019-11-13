a3440dcbc596325c63a3385cb817fbf9c5944317
Pavel Janda <me@paveljanda.com>
2019-11-13 10:52:04 +0100

    phpcs fix

--- src/DataSource/ArrayDataSource.php
+++ src/DataSource/ArrayDataSource.php
@@ -148,10 +148,10 @@ class ArrayDataSource implements IDataSource
 
 				$words = $filter instanceof FilterText && $filter->hasSplitWordsSearch() === false ? [$value] : explode(' ', $value);
 
-				$row_value = strtolower(Strings::toAscii((string)$row[$column]));
+				$row_value = strtolower(Strings::toAscii((string) $row[$column]));
 
 				foreach ($words as $word) {
-					if (strpos($row_value, strtolower(Strings::toAscii((string)$word))) !== false) {
+					if (strpos($row_value, strtolower(Strings::toAscii((string) $word))) !== false) {
 						return $row;
 					}
 				}
