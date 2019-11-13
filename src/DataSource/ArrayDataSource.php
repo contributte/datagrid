d912dc0e1501d094244f70981273ba19d43fb5ea
Martin Kuchynar <matokuchy@gmail.com>
2019-11-13 07:57:52 +0100

    Fix TypeError in ArrayDataSource.

--- src/DataSource/ArrayDataSource.php
+++ src/DataSource/ArrayDataSource.php
@@ -146,10 +146,10 @@ class ArrayDataSource implements IDataSource
 
 				$words = $filter instanceof FilterText && $filter->hasSplitWordsSearch() === false ? [$value] : explode(' ', $value);
 
-				$row_value = strtolower(Strings::toAscii($row[$column]));
+				$row_value = strtolower(Strings::toAscii((string)$row[$column]));
 
 				foreach ($words as $word) {
-					if (strpos($row_value, strtolower(Strings::toAscii($word))) !== false) {
+					if (strpos($row_value, strtolower(Strings::toAscii((string)$word))) !== false) {
 						return $row;
 					}
 				}
