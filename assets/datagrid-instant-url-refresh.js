f8479dc775dcadc5316b84c508a25457df93f9f5
Martin Brettschneider <martin.brettschneider@gmail.com>
2020-01-13 10:39:16 +0100

    datagrid-instant-url-refresh.js: dropped jQuery dependency

--- assets/datagrid-instant-url-refresh.js
+++ assets/datagrid-instant-url-refresh.js
@@ -2,8 +2,8 @@ var dataGridRegisterAjaxCall;
 
 if (typeof naja !== "undefined") {
 	dataGridRegisterAjaxCall = function (params) {
-        var method = params.type || 'GET';
-        var data = params.data || null;
+		var method = params.type || 'GET';
+		var data = params.data || null;
 
 		naja.makeRequest(method, params.url, data, {})
 			.then(params.success)
@@ -14,11 +14,13 @@ if (typeof naja !== "undefined") {
 	dataGridRegisterAjaxCall = $.nette.ajax;
 }
 
-$(function() {
-	if ($('.datagrid').length) {
+document.addEventListener('DOMContentLoaded', function () {
+	var element = document.querySelector('.datagrid');
+
+	if (element !== null) {
 		return dataGridRegisterAjaxCall({
 			type: 'GET',
-			url: $('.datagrid').first().data('refresh-state')
+			url: element.getAttribute('data-refresh-state')
 		});
 	}
 });
