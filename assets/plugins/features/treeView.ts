68e6f303b524f2e11954040329f58ad85aa6879f
Pavel Konvička <pavel.konvicka@gmail.com>
2025-01-22 01:44:22 +0100

    fix: treeview expand child nodes

--- assets/plugins/features/treeView.ts
+++ assets/plugins/features/treeView.ts
@@ -9,9 +9,18 @@ export class TreeViewPlugin implements DatagridPlugin {
 		datagrid.ajax.addEventListener("success", ({detail: {payload}}) => {
 			if (payload._datagrid_tree) {
 				const id = payload._datagrid_tree;
+				const rowBlock = document.querySelector(`.datagrid-tree-item[data-id="${id}"]`);
 				const childrenBlock = document.querySelector(`.datagrid-tree-item[data-id="${id}"] .datagrid-tree-item-children`);
 				if (childrenBlock) {
-					childrenBlock.classList.add('loaded');
+					if (childrenBlock.classList.contains('showed')) {
+						childrenBlock.innerHTML = '';
+						childrenBlock.classList.remove('showed');
+						rowBlock.querySelector(`a.chevron`).style.transform = "rotate(0deg)";
+						return;
+					}
+
+					childrenBlock.classList.add('showed');
+					rowBlock.querySelector(`a.chevron`).style.transform = "rotate(90deg)";
 					const snippets = payload.snippets;
 					for (const snippetName in snippets) {
 						const snippet = snippets[snippetName];
