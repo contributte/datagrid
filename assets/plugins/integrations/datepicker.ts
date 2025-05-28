2c93d64beacea4404d21010826ff03d305657b5f
Milan Felix Šulc <rkfelix@gmail.com>
2025-05-28 14:12:56 +0200

    Assets[datepicker]: reinit after success ajax

--- assets/plugins/integrations/datepicker.ts
+++ assets/plugins/integrations/datepicker.ts
@@ -6,12 +6,20 @@ export class DatepickerPlugin implements DatagridPlugin {
 	}
 
 	onDatagridInit(datagrid: Datagrid): boolean {
+		datagrid.ajax.addEventListener('complete', (event) => {
+			this.initDatepicker(datagrid);
+		});
+
+		this.initDatepicker(datagrid);
+
+		return true;
+	}
+
+	private initDatepicker(datagrid: Datagrid): void {
 		const elements = datagrid.el.querySelectorAll<HTMLInputElement>("input[data-provide='datepicker']");
 
 		if (elements.length >= 1) {
 			this.datepicker.initDatepickers(Array.from(elements), datagrid);
 		}
-
-		return true;
 	}
 }
