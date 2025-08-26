5cf92a69f6720e44550bf3a1644b0473a94bac1a
Veszter-007 <szilveszter.borsos@gmail.com>
2025-08-26 14:09:24 +0200

    Update confirm.ts
    Confirm is valid for the given grid, not global.

--- assets/plugins/features/confirm.ts
+++ assets/plugins/features/confirm.ts
@@ -5,21 +5,21 @@ export const ConfirmAttribute = "data-datagrid-confirm";
 
 export class ConfirmPlugin implements DatagridPlugin {
 	onDatagridInit(datagrid: Datagrid): boolean {
-        datagrid.el
-            .querySelectorAll<HTMLElement>(`[${ConfirmAttribute}]:not(.ajax)`)
-            .forEach(confirmEl =>
-                confirmEl.addEventListener("click", e => this.confirmEventHandler.call(datagrid, confirmEl, e))
-            );
+		datagrid.el
+			.querySelectorAll<HTMLElement>(`[${ConfirmAttribute}]:not(.ajax)`)
+			.forEach(confirmEl =>
+				confirmEl.addEventListener("click", e => this.confirmEventHandler.call(datagrid, confirmEl, e))
+			);
 
-        datagrid.ajax.addEventListener("interact", e => {
-            if (datagrid.el.contains(e.detail.element)) {
-                this.confirmEventHandler.call(datagrid, e.detail.element, e);
-            }
-        });
+		datagrid.ajax.addEventListener("interact", e => {
+			if (datagrid.el.contains(e.detail.element)) {
+				this.confirmEventHandler.call(datagrid, e.detail.element, e);
+			}
+		});
 
-        return true;
-    }
-	
+		return true;
+	}
+
 	confirmEventHandler(this: Datagrid, el: HTMLElement, e: Event) {
 		const message = el.closest('a')?.getAttribute(ConfirmAttribute)!;
 		if (!message) return;
